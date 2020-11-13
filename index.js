d1f461abab575fe31927a45d4785059bd1e1d45a
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-11-14 00:13:55 +0100

    Added run function

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const shell = require("shelljs");
+
+const run = async() => {
+    // show script introduction
+    // ask questions
+    // create the file
+    // show success message
+};
+
+run();
