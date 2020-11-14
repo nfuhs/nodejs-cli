8ed94932c1ea99edf4b5a261fef315e33444f177
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-11-14 21:13:25 +0100

    Added intro text

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const chalk = require("chalk");
 const figlet = require("figlet");
 const shell = require("shelljs");
 
+const init = () => {
+    console.log(
+        chalk.green(
+            figlet.textSync("Node f*cking JS", {
+                font: "Ghost",
+                horizontalLayout: "default",
+                verticalLayout: "default"
+            })
+        )
+    );
+}
+
 const run = async() => {
     // show script introduction
+    init();
+
     // ask questions
     // create the file
     // show success message
