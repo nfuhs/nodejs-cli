34ff27f0d68674b5b72b5b8ba587b71191747770
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-11-15 13:44:18 +0100

    added ask questions part

--- index.js
+++ index.js
@@ -17,11 +17,34 @@ const init = () => {
     );
 }
 
+const askQuestions = () => {
+    const questions = [{
+            name: "FILENAME",
+            type: "input",
+            message: "What is the name of the file without extension?"
+        },
+        {
+            type: "list",
+            name: "EXTENSION",
+            message: "What is the file extension?",
+            choices: [".rb", ".js", ".go", ".css"],
+            filter: function(val) {
+                return val.split(".")[1];
+            }
+        }
+    ];
+    return inquirer.prompt(questions);
+};
+
+
 const run = async() => {
     // show script introduction
     init();
 
     // ask questions
+    const answers = await askQuestions();
+    const { FIILENAME, EXTENSION } = answers;
+
     // create the file
     // show success message
 };
